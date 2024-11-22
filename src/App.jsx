3f92ff82fc705c1a08ef8890a1281347e26c3e16
himanigupta <gupta.himani44@gmail.com>
2024-11-22 04:17:52 -0800

    using ref for handling form data

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
+import { useRef } from "react";
 import "./App.css";
 
 const App = () => {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    console.log({
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    });
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit} className="user-form">
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="password" />
-        <button type="submit">Button</button>
+        <input type="text" placeholder="Name" ref={nameRef} />
+        <input type="email" placeholder="Email" ref={emailRef} />
+        <input type="password" placeholder="password" ref={passwordRef} />
+        <button type="submit">Submit Button</button>
       </form>
     </div>
   );
